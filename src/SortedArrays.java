1d6383d01c4168ccdbc90091cf797dab3594f027
xamide <Xamide33@gmail.com>
2018-11-15 00:56:46 +0200

    updated

--- src/SortedArrays.java
+++ src/SortedArrays.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class SortedArrays {
 
-    int [] sort (int[] a) {
+    int[] sort(int[] a) {
 
         int holder, i, j, c;
         List<Integer> arr = new ArrayList<>();
@@ -11,30 +11,27 @@ public class SortedArrays {
         // sort input array
         for (i = 0; i < a.length; i++) {
             for (j = 0; j < a.length; j++) {
-                if (a[0 + i] < a[j]) {
-                    holder = a[0 + i];
-                    a[0 + i] = a[j];
+                if (a[i] < a[j]) {
+                    holder = a[i];
+                    a[i] = a[j];
                     a[j] = holder;
                 }
             }
         }
 
         // add sorted array values into integer list
+        // remove duplicates from the list and add them to the array
         for (int b = 0; b < a.length; b++) {
             arr.add(a[b]);
-
-        }
-
-        // remove duplicates from the list and add them to the array
-        for (c = 0; c < arr.size(); c++) {
-            for (int v = c + 1; v < arr.size(); v++) {
-                if (arr.get(c).equals(arr.get(v))) {
+            for (int v = b; v < arr.size(); v++) {
+                if (arr.contains(arr.get(v))) {
                     arr.remove(v);
                 }
+
             }
         }
 
-        int [] sortedArray = new int [arr.size()];
+        int[] sortedArray = new int[arr.size()];
 
         for (int m = 0; m < arr.size(); m++) {
             sortedArray[m] = arr.get(m);
